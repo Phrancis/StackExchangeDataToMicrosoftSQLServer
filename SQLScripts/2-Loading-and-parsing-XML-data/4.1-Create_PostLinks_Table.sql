318da498e6e0f2ea1aca0ecb15d6965b85a07adf
Phrancis <Phrancis@users.noreply.github.com>
2016-09-17 09:32:17 -0400

    add 4.1-Create_PostLinks_Table.sql

---
+++ SQLScripts/2-Loading-and-parsing-XML-data/4.1-Create_PostLinks_Table.sql
@@ -0,0 +1,56 @@
+/*
+1.1-Create_RawDataXml_Comments_Table.sql
+
+1. Change database name if needed
+2. Execute script
+3. Table will be shown after completion
+*/
+
+-- Change database name if needed:
+USE SE
+GO
+-- Peripheral table
+IF OBJECT_ID('CleanData.PostLinkTypes') IS NOT NULL
+    DROP TABLE CleanData.PostLinkTypes;
+GO
+CREATE TABLE CleanData.PostLinkTypes (
+    Id TINYINT PRIMARY KEY,
+    Description NVARCHAR(256)
+);
+GO
+INSERT INTO CleanData.PostLinkTypes VALUES
+(1, 'Mention'),
+(3, 'Duplicate');
+GO
+
+-- Main table
+IF OBJECT_ID('CleanData.PostLinks') IS NOT NULL
+    DROP TABLE CleanData.PostLinks;
+GO
+CREATE TABLE CleanData.PostLinks (
+    SiteId UNIQUEIDENTIFIER NOT NULL,
+    ApiSiteParameter NVARCHAR(256) NOT NULL,
+    RowId INT,
+    CreationDate DATETIME2,
+    PostId INT,
+    RelatedPostId INT,
+    LinkTypeId TINYINT,
+    CONSTRAINT fk_PostLinks_PostLinkTypes FOREIGN KEY (LinkTypeId) REFERENCES CleanData.PostLinkTypes(Id)
+);
+GO
+CREATE NONCLUSTERED INDEX ix_PostLinks_PostId_RelatedPostId ON CleanData.PostLinks(PostId ASC, RelatedPostId ASC);
+CREATE NONCLUSTERED INDEX ix_PostLinks_ApiSiteParameter_SiteId ON CleanData.PostLinks(ApiSiteParameter ASC, SiteId);
+CREATE NONCLUSTERED INDEX ix_PostLinks_CreationDate ON CleanData.PostLinks(CreationDate ASC);
+GO
+
+-- Verification
+SELECT 
+    s.name AS [schema], 
+    t.name AS [table], 
+    c.name AS [column], 
+    c.column_id as [column_id]
+FROM sys.schemas AS s
+JOIN sys.tables AS t ON s.schema_id = t.schema_id
+JOIN sys.columns AS c ON t.object_id = c.object_id
+AND t.name IN ('PostLinks', 'PostLinksTypes')
+ORDER BY t.name, c.column_id
