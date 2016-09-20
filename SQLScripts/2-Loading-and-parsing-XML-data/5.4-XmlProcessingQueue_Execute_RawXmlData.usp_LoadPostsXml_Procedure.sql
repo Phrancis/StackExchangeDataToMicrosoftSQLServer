c300dfb90ec40b828cc823c54a0ab00cb33ebcfa
Phrancis <Phrancis@users.noreply.github.com>
2016-09-20 09:45:40 -0400

    add 5.4-XmlProcessingQueue_Execute_RawXmlData.usp_LoadPostsXml_Procedure.sql

---
+++ SQLScripts/2-Loading-and-parsing-XML-data/5.4-XmlProcessingQueue_Execute_RawXmlData.usp_LoadPostsXml_Procedure.sql
@@ -0,0 +1,108 @@
+USE SE
+GO
+SET NOCOUNT ON;
+GO
+
+-- Edit this to the number of Posts XML rows (i.e. files) you want to process:
+DECLARE @NumRowsToProcess INT = 100;
+
+-- Run the script
+-- NOTE THAT THIS SCRIPT CAN TAKE VERY LONG TO PROCESS FOR LARGE NUMBERS OF FILES.
+DECLARE @TotalRows INT = (
+    SELECT COUNT(*) 
+    FROM RawDataXml.XmlProcessingQueue AS q
+    WHERE q.DataType = 'Posts' 
+    AND q.Processed = 0
+    AND q.SiteDirectory IN (
+        SELECT Value
+        FROM RawDataXml.Globals
+        WHERE Parameter = 'TargetSite'
+    )    
+);
+IF @NumRowsToProcess > @TotalRows SET @NumRowsToProcess = @TotalRows;
+
+IF OBJECT_ID('tempdb..#RowsToProcess') IS NOT NULL
+DROP TABLE #RowsToProcess;
+
+CREATE TABLE #RowsToProcess (RowNum INT, ApiSiteParameter NVARCHAR(256));
+
+DECLARE @SQL NVARCHAR(MAX) = 
+'INSERT INTO #RowsToProcess(RowNum, ApiSiteParameter)' +
+'SELECT ' + 
+'TOP ' + CAST(@NumRowsToProcess AS VARCHAR(10)) + 
+' q.RowNum, q.ApiSiteParameter 
+FROM RawDataXml.XmlProcessingQueue AS q
+JOIN RawDataXml.Globals AS g
+    ON g.Value = q.SiteDirectory
+    AND g.Parameter = ''TargetSite''
+WHERE q.DataType = ''Posts''
+    AND q.Processed = 0
+    ORDER BY q.RowNum ASC;'
+
+EXECUTE sp_executesql @SQL;
+
+DECLARE @StartTime DATETIME2 = GETDATE()
+
+DECLARE 
+    @RowNum INT, 
+    @SiteDirectory NVARCHAR(256), 
+    @FullFilePath NVARCHAR(512),
+    @Now DATETIME2;
+
+DECLARE _PostsXmlProcessing CURSOR FOR
+    SELECT
+        RowNum, 
+        SiteDirectory, 
+        FilePath 
+    FROM RawDataXml.XmlProcessingQueue
+    WHERE RowNum IN (SELECT RowNum FROM #RowsToProcess)
+    ORDER BY RowNum ASC;
+
+OPEN _PostsXmlProcessing;
+
+FETCH NEXT FROM _PostsXmlProcessing INTO @RowNum, @SiteDirectory, @FullFilePath;
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    SET @Now = GETDATE();
+    EXECUTE RawDataXml.usp_LoadPostsXml @SiteDirectory, @FullFilePath;
+
+    INSERT INTO RawDataXml.XmlProcessingLog
+    SELECT SiteId, ApiSiteParameter, SiteDirectory, FilePath, 
+        DATEDIFF(MILLISECOND, @Now, GETDATE()), 
+        GETDATE()
+    FROM RawDataXml.XmlProcessingQueue
+    WHERE RowNum = @RowNum;
+    UPDATE RawDataXml.XmlProcessingQueue
+    SET Processed = 1
+    WHERE RowNum = @RowNum;
+
+    FETCH NEXT FROM _PostsXmlProcessing INTO @RowNum, @SiteDirectory, @FullFilePath;
+END
+
+CLOSE _PostsXmlProcessing;
+DEALLOCATE _PostsXmlProcessing;
+
+SELECT DATEDIFF(SECOND, @StartTime, GETDATE()) AS [ProcessingTimeSeconds]
+
+--verify
+SET NOCOUNT OFF;
+
+SELECT * 
+FROM RawDataXml.XmlProcessingLog
+WHERE FilePath like '%Posts%' 
+ORDER BY Processed ASC;
+
+SELECT * 
+FROM CleanData.Posts 
+ORDER BY Inserted ASC;
+
+SELECT COUNT(*) AS [PostsXmlLeftToProcess] 
+FROM RawDataXml.XmlProcessingQueue AS q
+WHERE q.DataType = 'Posts' 
+AND q.Processed = 0
+AND q.SiteDirectory IN (
+    SELECT Value
+    FROM RawDataXml.Globals
+    WHERE Parameter = 'TargetSite'
+);
